87d39fd6baa38e4629b77d1d778c83fe090a053a
saprasona <128086441+saprasona@users.noreply.github.com>
2023-03-17 00:01:17 +0530

    Add files via upload

---
+++ substraction.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+ 
+{
+	int a=55;
+	int b=25;
+	int c=a-b;
+	printf("%d\n", a);
+
+	printf("%d\n",b);
+
+	printf("substraction of a - b = c = %d \n",c);
+	
+	return 0;
+}
