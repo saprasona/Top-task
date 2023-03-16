5755e4dad3bbab922b7c3ed69a88c4cf1e3bb731
saprasona <128086441+saprasona@users.noreply.github.com>
2023-03-16 23:57:44 +0530

    Add files via upload

---
+++ switchcase_month.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+	int month = 12;
+	printf("enter the number");
+	scanf("%d",&month);
+	switch(month)
+	{  
+	    case 1:printf("january");
+		    break;
+		case 2:printf("february");
+			break;
+	    case 3:printf("march");
+	    	break;
+	    case 4:printf("april");
+			break;
+		case 5:printf("may");
+			break;
+		case 6:printf("june");
+			break;	
+		case 7:printf("july");
+			break;	
+		case 8:printf("august");
+			break;	
+		case 9:printf("september");
+			break;	
+		case 10:printf("october");
+			break;
+		case 11:printf("november");
+			break;	
+		case 12:printf("december");
+			break;	
+	default : printf("please enter valid number");
+	 		
+	}
+	
+	
+	return 0;
+}
