d864979d4fb9e803f75b531fc598a237336b2a3e
saprasona <128086441+saprasona@users.noreply.github.com>
2023-03-17 00:07:18 +0530

    Add files via upload

---
+++ myself.c
@@ -0,0 +1,11 @@
+#include<stdio.h> 
+int main() {
+    
+    printf("my name is sona sapra\n");
+    printf("my age is 35 year old\n");
+    printf("my birthdate is 15/10/1988\n");
+    printf("my gender is female\n");
+    printf("address:- 82, laxminagar society sarthana jakatnaka surat\n");
+    
+    return 0;
+}
---
+++ voidmain.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+    int a=50;
+
+    printf(" a=%d\n",a);
+
+
+}
